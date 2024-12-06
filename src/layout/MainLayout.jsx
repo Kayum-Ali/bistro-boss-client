999bfea1bc89b5df1effbc1e76aa62a11e322489
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-12-06 21:46:14 +0600

    navbar str done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../shared/Footer";
+import Navbar from "../shared/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div className="max-w-screen-[1920px]"> 
+     
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
             
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,8 +1,22 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="bg-[#151515c0]">
+            <div className="">
+                <h2>Bistro Boss </h2>
+                <h2>Resturent</h2>
+            </div>
+            <div>
+                <ul className="flex gap-4">
+                    <li><NavLink to={'/'}>Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/menu">Menu</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+            </div>
+            
             
         </div>
     );
