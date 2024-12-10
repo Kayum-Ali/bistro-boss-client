3aa154237e09b449deae62bd3cb113555823965c
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-12-10 10:37:25 +0600

    testimonials section done and featured section respnsive done

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -8,7 +8,7 @@ const Featured = () => {
     return (
         <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading="check it out" heading="Featured Item" ></SectionTitle>
-            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
+            <div className="flex flex-col md:flex-row lg:flex-row justify-center items-center bg-[rgba(21,21,21,0.7)] h-full pb-10 gap-8  lg:pb-20 lg:pt-12 pt-5 lg:px-36 px-5">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -16,7 +16,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita hic dolorem, iusto vel suscipit nam excepturi debitis magnam nostrum! Ut eum dignissimos culpa doloremque eligendi consectetur blanditiis laboriosam fugiat ea quia similique quam nisi reprehenderit numquam magnam nemo vitae cupiditate, atque maiores dicta minus pariatur. Perspiciatis nobis vero quas?</p>
-                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+                    <button className="btn btn-outline text-white border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
--- src/pages/Home/PopuplarMenu/PopularMenu.jsx
+++ src/pages/Home/PopuplarMenu/PopularMenu.jsx
@@ -5,30 +5,32 @@ import MenuItem from "../../../shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
     const [menu, setMenu] = useState([]);
-    useEffect(()=> {
+    useEffect(() => {
         fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const menuItems = data.filter(item => item.category === "popular");
-            setMenu(menuItems)
-        })
+            .then(res => res.json())
+            .then(data => {
+                const menuItems = data.filter(item => item.category === "popular");
+                setMenu(menuItems)
+            })
     }, [])
     return (
         <section className="mb-12 container mx-auto">
-            <SectionTitle 
-            subHeading={"Check it out"}
-            heading={"FROM OUR MENU"}
+            <SectionTitle
+                subHeading={"Check it out"}
+                heading={"FROM OUR MENU"}
             >
 
             </SectionTitle>
             <div className="grid md:grid-cols-2 gap-10 px-5">
-                
-                  {
+
+                {
                     menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-                  }
+                }
             </div>
-            <button className="btn btn-outline border-0 border-b-4 mt-4 ">View Full Menu</button>
-            
+            <div className="text-center">
+                <button className="btn btn-outline border-0 border-b-4 mt-4 ">View Full Menu</button>
+            </div>
+
         </section>
     );
 };
